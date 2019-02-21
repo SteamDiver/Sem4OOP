57fbe73a9d03074297bf0cd830ad456d46333fbb
SteamDiver <steamdiver@yandex.ru>
2019-02-22 00:39:02 +0300

    refactoring

---
+++ Task1/business.py
@@ -0,0 +1,20 @@
+def find_all(a, value):
+    return [i for i, j in enumerate(a) if j == value]
+
+def read_array(file):
+    with open(str(file), 'r') as f:
+        arr = [int(x) for x in f.readline().split(", ")]
+        return arr
+
+def process(arr):
+    first_max_index = find_all(arr, max(arr))[0]
+    last_min_index = find_all(arr, min(arr))[-1]
+    if first_max_index < last_min_index:
+        start_index = first_max_index
+        stop_index = last_min_index
+    else:
+        start_index = last_min_index
+        stop_index = first_max_index
+
+    sub_array = arr[start_index + 1:stop_index]
+    return arr[0: start_index + 1] + list(reversed(sub_array)) + arr[stop_index:]
